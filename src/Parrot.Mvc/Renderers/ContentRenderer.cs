ee2c2b753799dfdef5bb32b71ca8985c8c0e5abb
Ben Dornis <ben@buildstarted.com>
2012-08-07 13:53:37 -0700

    moved content/partial renderers to their own files

---
+++ src/Parrot.Mvc/Renderers/ContentRenderer.cs
@@ -0,0 +1,28 @@
+namespace Parrot.Mvc.Renderers
+{
+    using System;
+    using Nodes;
+
+    public class ContentRenderer : IRenderer
+    {
+        public string Render(AbstractNode node, object model)
+        {
+            dynamic localModel = model;
+
+            Document document = new Document
+            {
+                Children = localModel.Children
+            };
+
+            return document.Render(localModel.Model);
+        }
+
+
+
+        [Obsolete]
+        public string Render(AbstractNode node)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
