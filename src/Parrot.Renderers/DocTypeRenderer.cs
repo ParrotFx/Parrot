415fdcb8940b04b848a0edfdcf06b8e05f003295
Ben Dornis <ben@buildstarted.com>
2013-01-14 23:10:53 -0800

    added doctype support and script renderer

--- src/Parrot.Renderers/DocTypeRenderer.cs
+++ src/Parrot.Renderers/DocTypeRenderer.cs
@@ -9,6 +9,24 @@ namespace Parrot.Renderers
 
     public class DocTypeRenderer : BaseRenderer, IRenderer
     {
+        private static Dictionary<string, Func<string>> _docTypes;
+
+        static DocTypeRenderer()
+        {
+            _docTypes = new Dictionary<string, Func<string>>()
+                {
+                    {"html", () => "html"},
+                    {"html 4.01 strict", () => "HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\""},
+                    {"html 4.01 transitional", () => "HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\""},
+                    {"html 4.01 frameset", () => "HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\""},
+                    {"xhtml 1.0 strict", () => "html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"" },
+                    {"xhtml 1.0 transitional", () => "html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"" },
+                    {"xhtml 1.0 frameset", () => "html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\"" },
+                    {"xhtml 1.1", () => "html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\"" },
+                    {"xhtml 1.1 basic", () => "html PUBLIC \"-//W3C//DTD XHTML Basic 1.1//EN\" \"http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd\"" },
+                };
+        }
+
         public DocTypeRenderer(IHost host)
         {
             Host = host;
@@ -35,6 +53,13 @@ namespace Parrot.Renderers
                     value = result.ToString();
                 }
             }
+
+            if (_docTypes.ContainsKey(value))
+            {
+                value = _docTypes[value]();
+            }
+
+            documentHost.Add("doctype", value);
 
             writer.Write(string.Format("<!DOCTYPE {0}>", value));
         }
---
+++ src/Parrot.Renderers/ScriptRenderer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parrot.Renderers
+{
+    using Parrot.Renderers.Infrastructure;
+
+    public class ScriptRenderer : HtmlRenderer
+    {
+        public override IEnumerable<string> Elements { get { yield return "script"; } }
+        
+        public ScriptRenderer(IHost host) : base(host) { }
+
+
+        protected override void CreateTag(Parrot.Infrastructure.IParrotWriter writer, IRendererFactory rendererFactory, IDictionary<string, object> documentHost, object model, Nodes.Statement statement, IModelValueProvider modelValueProvider)
+        {
+            var xhtml = false;
+            if (documentHost.ContainsKey("doctype"))
+            {
+                //we have a registered doctype, is it xml?
+                if (documentHost["doctype"].ToString().IndexOf("xhtml", StringComparison.OrdinalIgnoreCase) != -1)
+                {
+                    //it's xhtml, we need to output cdata
+                    xhtml = true;
+                }
+            }
+
+
+            string tagName = string.IsNullOrWhiteSpace(statement.Name) ? DefaultChildTag : statement.Name;
+
+            TagBuilder builder = new TagBuilder(tagName);
+            //add attributes
+            RenderAttributes(rendererFactory, documentHost, model, statement, builder);
+            //AppendAttributes(builder, statement.Attributes, documentHost, modelValueProvider);
+
+            writer.Write(builder.ToString(TagRenderMode.StartTag));
+            //render children
+
+            if (xhtml)
+            {
+                writer.Write("//<![CDATA[");
+            }
+
+            if (statement.Children.Count > 0)
+            {
+                RenderChildren(writer, statement, rendererFactory, documentHost, model);
+            }
+
+            if (xhtml)
+            {
+                writer.Write("//]]>");
+            }
+
+            writer.Write(builder.ToString(TagRenderMode.EndTag));
+        }
+    }
+}
