c2d2739b388d837bb37e7b5066f8810ca454172d
Ben Dornis <ben@buildstarted.com>
2013-06-07 12:36:23 -0700

    Cleanup
    * Modified IfRenderer formatting

--- src/Parrot.Renderers/IfRenderer.cs
+++ src/Parrot.Renderers/IfRenderer.cs
@@ -6,10 +6,7 @@
 
     public class IfRenderer : HtmlRenderer
     {
-        public IfRenderer(IHost host)
-            : base(host)
-        {
-        }
+        public IfRenderer(IHost host) : base(host) { }
 
         public override IEnumerable<string> Elements
         {
@@ -18,7 +15,6 @@
 
         public override void Render(Parrot.Infrastructure.IParrotWriter writer, IRendererFactory rendererFactory, Nodes.Statement statement, IDictionary<string, object> documentHost, object model)
         {
-            //base.Render(writer, rendererFactory, statement, documentHost, model);
             Type modelType = model != null ? model.GetType() : null;
             var modelValueProvider = Host.ModelValueProviderFactory.Get(modelType);
 
